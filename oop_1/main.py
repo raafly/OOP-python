48f3c67ccfdbc7c1a2714d1ab01c419989ab8e18
Agung <agung@agung.com>
2023-02-02 13:57:19 +0700

    OOP Pyhon

---
+++ oop_1/main.py
@@ -0,0 +1,19 @@
+class Hero: # template
+    pass
+
+hero1 = Hero() # object
+hero2 = Hero() 
+hero3 = Hero() 
+
+hero1.nama = "sniper"
+hero1.health = 100
+
+hero2.name = "heale"
+hero2.health = 50
+
+hero3.name = "xsara"
+hero3.health = 1000
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.nama)
---
+++ oop_2/main.py
@@ -0,0 +1,25 @@
+# class Hero: # template
+   
+#     def __init__(self,inputName, inputPower, inputArmor, inputHealth):
+#         self.name = inputName
+#         self.power = inputPower
+#         self.armor = inputArmor
+#         self.health = inputHealth
+
+# hero1 = Hero("sniper",100,10,4)
+# hero2= Hero("jakxc",100,15,0)
+
+# print(hero1.__dict__)
+# print(hero2.__dict__)
+
+class Maba:
+    def __init__(self,InputNama,InputLulusan, inputSkor):
+        self.nama = InputNama
+        self.lulusan = InputLulusan
+        self.skor = inputSkor
+
+maba1 = Maba("rafly", "smkn 8 semarang", 100)
+maba2 = Maba("ily", "smkn 8 semarang", 100)
+maba3 = Maba("xaxa", "smkn 8 semarang", 100)\
+
+print(maba1.__dict__)
